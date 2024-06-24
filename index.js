fb9f77e4912321761c551112533f22acd7320981
KilaniH <kilani.halim@gmail.com>
2024-06-24 09:17:36 +0100

    monApi

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server"); // import json-server library
+const server = jsonServer.create();
+const router = jsonServer.route();
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001; any port number Headers
+
+server.use(middlewares);
+serveruse(router);
+
+server.listen(port);
