fc750ab0ff316842c46f8efd9377c30bf6cc1848
KilaniH <67108172+KilaniH@users.noreply.github.com>
2024-06-24 10:12:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server"); // import json-server library
 const server = jsonServer.create();
-const router = jsonServer.route();
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // any port number Headers
 
